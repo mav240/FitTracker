6d514bd3d977da2ea95ad3c8b68fa5ff0fc2eed7
Miguel Vertiz <mav240@txstate.edu>
2023-04-04 15:38:25 -0500

    Added generated source files from Android Studio / created plash screen
    Initially created source code from Android Studio IDE and added page for splash screen

---
+++ app/src/main/java/com/fittracker/Splashscreenok.java
@@ -0,0 +1,13 @@
+package com.fittracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreenok extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreenok);
+    }
+}
