e083805b21875e253571cd9302428e3a767e8348
faithhshaffer <103451825+faithhshaffer@users.noreply.github.com>
2023-04-06 14:58:08 -0500

    Added Nutrition Tips option in main menu

--- app/src/main/java/com/fittracker/MainActivity.java
+++ app/src/main/java/com/fittracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.fittracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     //Testing
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Beginner(View view) {
+    }
+
+    public void Intermediate(View view) {
+    }
+
+    public void Pro(View view) {
+    }
+
+    public void food(View view) {
     }
 }
