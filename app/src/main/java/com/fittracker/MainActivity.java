369ccc0906f61be611d1f9906836c8797e508069
faithhshaffer <103451825+faithhshaffer@users.noreply.github.com>
2023-04-06 15:56:53 -0500

    Added activity for menu options

--- app/src/main/java/com/fittracker/MainActivity.java
+++ app/src/main/java/com/fittracker/MainActivity.java
@@ -1,27 +1,81 @@
 package com.fittracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1, button2, button3;
+
     //Testing
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        button1 = findViewById(R.id.startBeginner);
+        button2 = findViewById(R.id.startIntermediate);
+        button3 = findViewById(R.id.startPro);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
+
+
     public void Beginner(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
     }
 
     public void Intermediate(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
     }
 
     public void Pro(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
     }
 
     public void food(View view) {
+        Intent intent = new Intent(MainActivity.this, FoodActivity.class);
+        startActivity(intent);
     }
 }
