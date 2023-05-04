c707e56067c53a3e95b10228ffa7713d32b66c4b
waltonm280 <waltonm280@gmail.com>
2023-05-03 19:42:06 -0500

    added corner menu

--- app/src/main/java/com/fittracker/SecondActivity2.java
+++ app/src/main/java/com/fittracker/SecondActivity2.java
@@ -1,8 +1,11 @@
 package com.fittracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class SecondActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class SecondActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second2);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.id_privacy){
+            return true;
+        }
+        if(id == R.id.id_term){
+            return true;
+        }
+        if(id == R.id.rate){
+            return true;
+        }
+        if(id == R.id.more){
+            return true;
+        }
+        if(id == R.id.share){
+            return true;
+        }
+        return true;
     }
 }
